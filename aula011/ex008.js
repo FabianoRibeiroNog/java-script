300508407d7dc37408ecf589f8f3a85d7c7ece76
FabianoRibeiroNog <fabianoribeironog@gmail.com>
2025-06-12 16:10:08 -0300

    up

---
+++ aula011/ex008.js
@@ -0,0 +1,3 @@
+let vel = 60.5
+console.log(`A velocidade do seu carro é: ${vel}km/h`)
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula012/ex012.js
@@ -0,0 +1,11 @@
+let hora = 0
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <= 18) {
+    console.log('Boa tarde!')
+} else if (hora <= 24) {
+    console.log('Boa noite!')
+} else {
+    console.log('Boa Madrugada!')
+}
