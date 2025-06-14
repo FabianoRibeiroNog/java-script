874b2c184a7c40a82dba9bd06f4db9d05a52fe92
FabianoRibeiroNog <fabianoribeironog@gmail.com>
2025-06-14 01:40:26 -0300

    up

--- aula012.ex/ex014/script.js
+++ aula012.ex/ex014/script.js
@@ -3,17 +3,26 @@ function carregar() {
     let img = window.document.getElementById('imagem')
     let data = new Date()
     let hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+
+    if (hora >= 0 && hora <= 6) {
+        //BOA MADRUGADA
+        msg.innerHTML = `Agora são ${hora} horas da madrugada.`
+        img.src = 'imgs/madrugada.png'
+        document.body.style.background = '#292c5a'
+    } else if (hora > 6 && hora < 12) {
         //BOM DIA
+        msg.innerHTML = `Agora são ${hora} horas da manhã`
         img.src = 'imgs/manha.png'
-        document.body.style.background = '#f3e6ba'
-    } else if (hora >= 12 && hora <= 18) {
-        //BOA TARDE
+        document.body.style.background = '#885d4c'
+    } else if (hora >= 12 && hora < 18) {
+        msg.innerHTML = `Agora são ${hora} horas da tarde`
         img.src = 'imgs/tarde.png'
         document.body.style.background = '#e7e8ea'
-    } else {
+    } else if (hora >= 18 && hora <= 24) {
+        msg.innerHTML = `Agora são ${hora} horas da noite`
         img.src = 'imgs/noite.png'
-        document. body.style.background = '#515154'
+        document.body.style.background = '#515154'
+    } else {
+        msg.innerHTML = `[ERRO] está hora está incorreta, tente novamente!!!`
     }
 }
